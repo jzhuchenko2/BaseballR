662fe5a82d05c2c71196e32d7a6f4cb6b1d4dba3
Joshua Zhuchenko <105894219+jzhuchenko2@users.noreply.github.com>
2023-10-13 01:26:14 -0500

    add the initial merge of statcast and batting sal from OOTP

--- THE.R
+++ THE.R
@@ -12,3 +12,7 @@ merged_batting_subset <- merged_batting[order(-merged_batting$ab), ][1:100, ]
 final_batting_data <- merged_batting_subset
 
 write.csv(final_batting_data, "final_batting_data.csv", row.names = FALSE)
+
+##pitchingsal$Name <- pitchingsal$`Name `
+
+merged_batting <- merge(stats_1_, battingsal, by="Name")
