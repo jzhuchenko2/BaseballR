eea4788fa82d3f7d98361d5a5cf54fb1f6536fb8
Joshua Zhuchenko <105894219+jzhuchenko2@users.noreply.github.com>
2023-10-13 01:24:26 -0500

    Create THE.R

---
+++ THE.R
@@ -0,0 +1,10 @@
+#stuff4_ <- merge(Pitching, Batting, by="playerID")
+#stuff4.1_ <- merge(People, battingsala, by="playerID")
+#stuff5_ <- merge(stuff4.1_, stuff4_, by="playerID")
+
+#combine stats 
+#batting stats
+merged_batting$pa <- merged_batting$pa / 100
+merged_batting$ab <- merged_batting$hit - merged_batting$pa
+merged_batting$head(100)
+merged_batting_subset <- merged_batting[order(-merged_batting$ab), ][1:100, ]
