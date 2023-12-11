c0a069b1a106bd465a28071466caa35d6bc15c91
Joshua Zhuchenko <105894219+jzhuchenko2@users.noreply.github.com>
2023-12-11 11:16:14 -0600

    Update Presentation.R
    addiong the spray chart for Raleigh

--- Presentation.R
+++ Presentation.R
@@ -20,3 +20,23 @@ Raleigh_bip <- Raleigh %>%
 Raleigh_bip %>%
   select(events, hc_x, hc_y) %>%
   head()
+
+## ------------------------------------------------------------------------
+# ggspraychart(data = correa, fill_value = "events")
+spray_chart <- function(...) {
+  ggplot(...) + 
+    geom_curve(x = 33, xend = 223, y = -100, yend = -100,
+               curvature = -.65) +
+    geom_segment(x = 128, xend = 33, y = -208, yend = -100) +
+    geom_segment(x = 128, xend = 223, y = -208, yend = -100) +
+    geom_curve(x = 83, xend = 173, y = -155, yend = -156,
+               curvature = -.65, linetype = "dotted") +
+    coord_fixed() + 
+    scale_x_continuous(NULL, limits = c(25, 225)) + 
+    scale_y_continuous(NULL, limits = c(-225, -25))
+}
+
+## ----spray_chart, warning=FALSE, fig.cap="Spray chart of balls in play hit by Carlos Correa in May, 2017."----
+spray_chart(Raleigh_bip, aes(x = hc_x, y = -hc_y, color = events)) + 
+  geom_point() + 
+  scale_color_grey()
